6d6aeffeb1ef51ca080fb1174e3a4411fd828783
Iustin Stolniceanu <stolniceanuiustin@gmail.com>
2026-09-03 16:38:44 +0300

    samples: daq2: add AD9523-1 clock bring-up node
    Add the DAQ2 sample overlay and prj.conf wiring the AD9523-1 on PS SPI0
    (the controller the DAQ2 bitstream routes to the FMC), 3-wire, explicit
    PLL2 divider path: VCXO 125 MHz, NDIV 24 -> VCO 3.0 GHz, M1 3 ->
    VCO1 1.0 GHz. PLL2 lock confirmed on hardware.
    
    Signed-off-by: Iustin Stolniceanu <iustin.stolniceanu@analog.com>

---
+++ samples/daq2/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("DAQ2 hello world! %s\n", CONFIG_BOARD_TARGET);
+
+	return 0;
+}
